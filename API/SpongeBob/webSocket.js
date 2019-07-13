de2d8f7cf0ed362cab01dc352b4173328f15afe5
hippo.wu <hippo.wu@antspace.com>
2019-07-13 16:26:39 +0800

    add websocket

--- API/SpongeBob/webSocket.js
+++ API/SpongeBob/webSocket.js
@@ -6,10 +6,9 @@ let response = require('../../public/response');
 const ws = require("nodejs-websocket")
 
 /**
- * 发送消息
+ * 储存数据到数据库
  */
-router.post('/send', (rep, res) => {
-    let body = rep.body
+let layMessages = (body) => {
     let new_messages = new (Parse.Object.extend('chat_messages'))
     let param = {
         send_user: {
@@ -25,91 +24,60 @@ router.post('/send', (rep, res) => {
         content: body.content
     }
 
-    new_messages.set(param)
+    return new_messages.set(param)
         .save()
         .then(data => {
-            res.send(response.success(data))
+            if (data) {
+                return data
+            } else {
+                return false
+            }
         })
-})
+
+}
 
 
-var str1 = null, str2 = null, clientReady = false, serverReady = false;
-var a = [];
-// 广播通知
-// const broadcast = (server, info) => {
-//     console.log('broadcast', info)
-//     server.connections.forEach(function(conn) {
-//       conn.sendText(JSON.stringify(info))
-//     })
-//   }
-var server = ws.createServer(function (conn) {
-    console.log(conn)
-    conn.on('connection', (wss) => {
-        console.log(wss)
-        wss.on('message', (ws, req) => {
-            // const ip = req.connection.remoteAddress;
-            console.log('ws', ws)
-            console.log('ip', req)
+
+
+var user = {};
+var content = {}
+var server = ws.createServer((conn) => {
+    let id = conn.path.substr(1, conn.path.length - 1)
+    user[id] = conn
+    // if (!content[id]) {
+    //     content[id] = []
+    // } else {
+    //     if (content[id].length) {
+    //         user[id].sendText(content[id])
+    //     }
+    // }
+    console.log(user);
+    conn.on('text', (req) => {
+        let param = JSON.parse(req)
+        layMessages(param).then(data => {
+            if (data) {
+                console.log(data)
+                console.log(param.send_user)
+                console.log(user[param.send_user])
+                if (user[param.receive_user]) {
+                    user[param.receive_user].sendText(data)
+                } else {
+                    content[param.receive_user].push(data)
+                }
+            } else {
+                user[param.send_user] ? user[param.send_user].sendText(response.fail('数据库储存失败')) : null
+            }
         })
     })
-   
-    // conn.on('text', function (str) {
-    //     console.log(str)
-    //     a.push(str);
-    //     if (!clientReady) {
-    //         if (a[0] === str) {
-    //             str1 = conn;
-    //             clientReady = true;
-    //             str1.sendText("欢迎你" + str);
 
-    //         }
-    //     } else if (!serverReady) {
-    //         if (str.indexOf('close') >= 0) {
-    //             a.splice(2, 1);
-    //             clientReady = false;
-    //             str1 = null;
-    //             return;
-    //         }
-    //         if (a[1] === str) {
-    //             str2 = conn;
-    //             serverReady = true;
-    //             str2.sendText("欢迎你" + str);
-    //             str1.sendText(str + "在线啦，你们可以聊天啦");
-    //             return;
-    //         }
-    //     } else if (clientReady && serverReady) {
-    //         str2.sendText(str);
-    //         str1.sendText(str);
-    //         if (str.indexOf('close') >= 0) {
-    //             a.splice(2, a.length);
-    //             var len = a.length;
-    //             for (var i = 0; i < len; i++) {
-    //                 // 定位该元素位置
-    //                 if (str.indexOf(a[i]) >= 0) {
-    //                     a.splice(i, 1);
-    //                     if (i == 0) {
-    //                         str1 = str2;
-    //                     }
-    //                     serverReady = false;
-    //                     str2 = null;
-    //                     return;
-    //                 }
-
-    //             }
-    //         }
-    //     }
-
-
-    // })
-
-    // conn.on("close", function (code, reason) {
-    //     console.log("关闭连接");
-    //     clientReady = false;
-    //     serverReady = false;
-    // })
-    // conn.on("error", function (code, reason) {
-    //     console.log("异常关闭");
-    // });
+    conn.on("close", function (code, reason) {
+        user[id] = null
+        console.log("关闭连接",user);
+    })
+    conn.on("error", function (code, reason) {
+        user[id] = null
+        console.log("异常关闭",user);
+    });
 });
 server.listen(8082, () => { console.log("websocket连接完毕") })
 
