e78a2a6f2646b58867f35c1951aeba044feeb89c
HIPPOWUYUFAN <hippo@hippodeMacBook-Pro.local>
2020-11-29 03:02:57 +0800

    1

--- service/im_service/ws.js
+++ service/im_service/ws.js
@@ -1,15 +1,5 @@
 var router = require('express').Router();
 const http = require("http").createServer(router)
-//跨域全局配置
-const allowCrossDomain = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Content-Type', "application/json; charset=utf-8");
-    next();
-};
-http.use(allowCrossDomain)
 var io = require('socket.io')(http);
 // var _ = require('underscore');
 
